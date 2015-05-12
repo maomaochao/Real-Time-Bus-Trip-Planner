68326ad536b1b364c46e4f81459afae125435fd8
jaly50 <jialic@andrew.cmu.edu>
2015-05-11 22:38:17 -0400

    logout

---
+++ src/controller/LogoutAction.java
@@ -0,0 +1,43 @@
+/*
+ * 08-600
+ * Homework #9
+ * Jiali Chen
+ * andrewID: jialic
+ * Dec 4, 2014
+ */
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.genericdao.RollbackException;
+
+import model.Model;
+import model.UserDAO;
+
+/*
+ * Logs out by setting the "user" session attribute to null.
+ * (Actions don't be much simpler than this.)
+ */
+public class LogoutAction extends Action {
+	
+	
+
+	public LogoutAction(Model model) {
+	}
+
+	public String getName() {
+		return "logout.do";
+	}
+
+	public String perform(HttpServletRequest request) {
+			HttpSession session = request.getSession(false);
+			session.setAttribute("user", null);
+
+			return "welcome.do";
+	
+	}
+}
